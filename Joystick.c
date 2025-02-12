a6a332dca8946e5a0c42900187029d2a637bd622
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-02-12 11:45:12 -0300

    inicializando os botões

--- Joystick.c
+++ Joystick.c
@@ -9,15 +9,27 @@
 #define LED_BLUE 12
 #define LED_RED 13
 
+// Definindo os pinos dos botões
+#define BUTTON_A 5
+#define BUTTON_JOYSTICK 22
+
 // Função para configurar os pinos GPIO
 void config_gpio() {
+    // Inicializando e configurando LEDs como saída
     gpio_init(LED_GREEN);
     gpio_init(LED_BLUE);
     gpio_init(LED_RED);
-
     gpio_set_dir(LED_GREEN, GPIO_OUT);
     gpio_set_dir(LED_BLUE, GPIO_OUT);
     gpio_set_dir(LED_RED, GPIO_OUT);
+
+    // Inicializando os botões como entrada e resistores pull-up
+    gpio_init(BUTTON_A);
+    gpio_init(BUTTON_JOYSTICK);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_set_dir(BUTTON_JOYSTICK, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+    gpio_pull_up(BUTTON_JOYSTICK);
 }
 
 int main() {
