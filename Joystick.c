87d6dfe0e2fcc602dedcea1c9dd0ed06fad96a12
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-02-12 11:43:43 -0300

    definindo os pinos para os leds e configurando inicialização dos gpios

--- Joystick.c
+++ Joystick.c
@@ -4,9 +4,25 @@
 #include "hardware/i2c.h"
 #include "hardware/pwm.h"
 
+// Definindo os pinos do LED RGB
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+
+// Função para configurar os pinos GPIO
+void config_gpio() {
+    gpio_init(LED_GREEN);
+    gpio_init(LED_BLUE);
+    gpio_init(LED_RED);
+
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+}
 
 int main() {
     stdio_init_all();
+    config_gpio();
 
     while (true) {
     }
