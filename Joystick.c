63e4e18a6b29f9f2d96f6653da09b249450b683b
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-02-12 11:37:57 -0300

    first commit

---
+++ Joystick.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "hardware/pwm.h"
+
+
+int main() {
+    stdio_init_all();
+
+    while (true) {
+    }
+}
